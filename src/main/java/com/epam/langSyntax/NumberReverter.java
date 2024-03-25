7e79116727c1a06c6227aaae400d0a95963b0219
Alex Pumnea <alexandr.pumnea@gmail.com>
2024-03-25 13:21:18 +0200

    Implement number reversal method
    The method to revert integers is implemented in the NumberReverter class. The incoming number is converted to a string, then reversed, and finally converted back to an integer.

--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -3,6 +3,9 @@ package com.epam.langSyntax;
 public class NumberReverter {
 
     public void revert(int number) {
+        String numAsStr = Integer.toString(number);
+        String reversedNumAsStr = new StringBuilder(numAsStr).reverse().toString();
+        int result = Integer.parseInt(reversedNumAsStr);
     }
 
 }
