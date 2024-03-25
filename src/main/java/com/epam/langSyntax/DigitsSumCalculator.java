726f5bc7288300a852338b1ea8cde755df9a3931
Alex Pumnea <alexandr.pumnea@gmail.com>
2024-03-25 13:23:25 +0200

    Implement digits sum calculator method
    The method to calculate the sum of digits in a number is implemented in the DigitsSumCalculator class. The incoming number is converted to a stream of chars and finally summed;

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -3,6 +3,8 @@ package com.epam.langSyntax;
 public class DigitsSumCalculator {
 
     public void calculateSum(int number) {
+        int result = String.valueOf(number).chars()
+                .map(Character::getNumericValue).sum();
     }
 
 }
