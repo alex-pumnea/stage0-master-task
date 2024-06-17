45c03eddcd23225664b5137d74d8036dfe8422a4
Alex Pumnea <alexandr.pumnea@gmail.com>
2024-06-17 09:48:43 +0300

    Implement triangle sides validation method.

--- src/main/java/com/epam/conditions/TriangleSidesValidator.java
+++ src/main/java/com/epam/conditions/TriangleSidesValidator.java
@@ -3,7 +3,19 @@ package com.epam.conditions;
 public class TriangleSidesValidator {
 
     public void validate(double firstSide, double secondSide, double thirdSide) {
+        if (checkValidity(firstSide, secondSide, thirdSide) == 1) {
+            System.out.println("this is a valid triangle");
+        } else {
+            System.out.println("it's not a triangle");
+        }
+    }
 
+    private int checkValidity(double firstSide, double secondSide, double thirdSide) {
+        if (firstSide + secondSide <= thirdSide || firstSide + thirdSide <= secondSide || secondSide + thirdSide <= firstSide) {
+            return 0;
+        } else {
+            return 1;
+        }
     }
 
 }
